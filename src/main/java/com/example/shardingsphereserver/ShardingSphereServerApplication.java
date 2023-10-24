95602c9f9ec6c1406ae5e2ba4026c96aa1eaf362
sunrisehouse <ajtwlswjddnv1102@gmail.com>
2023-10-24 06:51:48 +0000

    feat:initial commit

---
+++ src/main/java/com/example/shardingsphereserver/ShardingSphereServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shardingsphereserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingSphereServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingSphereServerApplication.class, args);
+	}
+
+}
